0b0f22eac816ca3d9b6699dd7ed2f14ec491a19a
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-08-17 11:20:41 +0530

    finding contours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#this is a random change
 import cv2
 import numpy as np
+
 
 def stackImages(scale,imgArray):
     rows = len(imgArray)
@@ -33,14 +33,29 @@ def stackImages(scale,imgArray):
         ver = hor
     return ver
 
-path = 'assets/shapes.png'
-img = cv2.imread(path)
-imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
+#creating contours
 
-imgStack = stackImages(0.6, (img, [imgGray, imgBlur]))
-#cv2.imshow('Original', img)
-#cv2.imshow('Gray Image', imgGray)
-#cv2.imshow('Blue Image', imgBlur)
-cv2.imshow('Stacked', imgStack)
+def getContours(img):
+    contours, heirarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        #for each contour find the area first
+        area = cv2.contourArea(cnt)
+        print(area)
+        cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
+        
+
+
+
+
+
+path = 'assets/shapes.png'
+img = cv2.imread(path) 
+imgContour = img.copy() 
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
+imgBlur = cv2.GaussianBlur(imgGray,(7,7),1) 
+imgCanny = cv2.Canny(imgBlur,50,50) 
+getContours(imgCanny)
+imgBlank = np.zeros_like(img) 
+imgStack = stackImages(0.8,([img,imgGray,imgBlur], [imgCanny,imgContour,imgBlank])) 
+cv2.imshow("Stack", imgStack) 
 cv2.waitKey(0)
